d071aad399e10b57ea1f41c71215ce489a03f9fa
Andrés Ortega <elarcanista@gmail.com>
2018-08-15 22:31:00 -0500

    Added documentation

--- Regex.hs
+++ Regex.hs
@@ -1,9 +1,22 @@
+{- | This module serves as an easy way to represent regular expressions. -}
 module Regex where
 
-data Regex a = Empty
+-- | Represents a regular expession with symbols of type "a"
+data Regex a =
+  -- | The empty language
+  Empty
+  -- | The empty string
   | Epsilon
+  {- | Represents a symbol of type "a" in the expression.
+       E.g. Symbol "a" = a -}
   | Symbol a
+  {- | Kleene closure of a regular language.
+       E.g. Star (Symbol "a") = a* -}
   | Star (Regex a)
+  {- | Union of two regular languages.
+       E.g. Plus (Symbol "a") (Symbol "b") = a+b -}
   | Plus (Regex a) (Regex a)
+  {- | Concatenation of two regular languages.
+       E.g. Dot (Symbol "a") (Symbol "b") = ab -}
   | Dot  (Regex a) (Regex a)
   deriving Show

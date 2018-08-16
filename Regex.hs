a9638e6772797ffc15fad260d9d0c194a74b0fe8
Andrés Ortega <elarcanista@gmail.com>
2018-08-15 23:10:34 -0500

    Regular language -> Regular expression

--- Regex.hs
+++ Regex.hs
@@ -10,13 +10,13 @@ data Regex a =
   {- | Represents a symbol of type "a" in the expression.
        E.g. Symbol "a" = a -}
   | Symbol a
-  {- | Kleene closure of a regular language.
+  {- | Kleene closure of a regular expression.
        E.g. Star (Symbol "a") = a* -}
   | Star (Regex a)
-  {- | Union of two regular languages.
+  {- | Union of two regular expressions.
        E.g. Plus (Symbol "a") (Symbol "b") = a+b -}
   | Plus (Regex a) (Regex a)
-  {- | Concatenation of two regular languages.
+  {- | Concatenation of two regular expressions.
        E.g. Dot (Symbol "a") (Symbol "b") = ab -}
   | Dot  (Regex a) (Regex a)
   deriving Show

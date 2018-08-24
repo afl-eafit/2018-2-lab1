0b2af8e1a2016fc43723f90d894d9c1f6319add4
Andrés Ortega <elarcanista@gmail.com>
2018-08-24 10:10:46 -0500

    Added pretty print to Regex

--- Regex.hs
+++ Regex.hs
@@ -1,3 +1,4 @@
+
 {- | This module serves as an easy way to represent regular expressions. -}
 module Regex where
 
@@ -19,4 +20,37 @@ data Regex a =
   {- | Concatenation of two regular expressions.
        E.g. Dot (Symbol "a") (Symbol "b") = ab -}
   | Dot  (Regex a) (Regex a)
-  deriving Show
+
+-- | Creates a String that is the infix representation of a Regex
+show' :: (Show c) => Regex c -> String
+show' Empty = "0"
+show' Epsilon = "1"
+show' (Symbol a) = show a
+show' (Star (Symbol a)) = show' (Symbol a) ++ "*"
+show' (Star a) = "(" ++ show' a ++ ")*"
+show' (Plus a b) = show' a ++ "+" ++ show' b
+show' (Dot (Symbol a) (Symbol b)) = show' (Symbol a) ++ show' (Symbol b)
+show' (Dot (Star a) (Star b)) = show' (Star a) ++ show' (Star b)
+show' (Dot (Symbol a) (Star b)) = show' (Symbol a) ++ show' (Star b)
+show' (Dot (Star a) (Symbol b)) = show' (Star a) ++ show' (Symbol b)
+show' (Dot (Symbol a) (Dot b c)) = show' (Symbol a) ++ show' (Dot b c)
+show' (Dot (Dot a b) (Symbol c)) = show' (Dot a b) ++ show' (Symbol c)
+show' (Dot (Star a) (Dot b c)) = show' (Star a) ++ show' (Dot b c)
+show' (Dot (Dot a b) (Star c)) = show' (Dot a b) ++ show' (Star c)
+show' (Dot (Symbol a) b) = show' (Symbol a) ++ "(" ++ show' b ++ ")"
+show' (Dot a (Symbol b)) = "(" ++ show' a ++ ")" ++ show' (Symbol b)
+show' (Dot (Star a) b) = show' (Star a) ++ "(" ++ show' b ++ ")"
+show' (Dot a (Star b)) = "(" ++ show' a ++ ")" ++ show' (Star b)
+show' (Dot (Dot a b) c) = show' (Dot a (Dot b c))
+show' (Dot a b) = "(" ++ (show' a) ++ ")(" ++ (show' b) ++ ")"
+
+-- | Erases all instances of a Char in a String
+erase :: Char -> String -> String
+erase _ [] = []
+erase c (h:t)
+  | c == h = erase c t
+  | otherwise = h : erase c t
+
+-- | String with the infix representation of a Regex (no \")
+instance (Show c) => Show (Regex c) where
+  show a = erase '\"' (show' a)
